438dccc77e50dfba8ef928ea5f11dd0e9fead0b6
Shigeki-Kimura <b1013173@fun.ac.jp>
2015-11-25 23:55:53 +0900

    MainActivityの一時停止機能の実装，ボタンの配色変更

--- app/src/main/java/com/example/b1013_000/demoactivity/MainActivity.java
+++ app/src/main/java/com/example/b1013_000/demoactivity/MainActivity.java
@@ -138,7 +138,7 @@ public class MainActivity extends Activity {
                         ((Chronometer) findViewById(R.id.chronometer)).start();
                         startFlag = true;
                         pauseFlag = true;
-                        startBt.setBackgroundColor(Color.rgb(255, 215, 0));
+                        startBt.setBackgroundColor(Color.rgb(253, 211, 92));
                         startBt.setText("一時停止する");
                         break;
                     }
@@ -146,7 +146,7 @@ public class MainActivity extends Activity {
                     if(startFlag && pauseFlag) {
                         ((Chronometer)findViewById(R.id.chronometer)).stop();
                         pauseFlag = false;
-                        startBt.setBackgroundColor(Color.rgb(144, 238, 144));
+                        startBt.setBackgroundColor(Color.rgb(121, 192, 110));
                         startBt.setText("計測を再開する");
                         break;
                     }
@@ -165,10 +165,11 @@ public class MainActivity extends Activity {
                         }
                         mChronometer.setBase(SystemClock.elapsedRealtime() - stoppedMilliseconds);
                         mChronometer.start();
-                        startBt.setBackgroundColor(Color.rgb(255, 215, 0));
+                        startBt.setBackgroundColor(Color.rgb(253, 211, 92));
                         startBt.setText("一時停止する");
                         startFlag = true;
                         pauseFlag = true;
+                        break;
                     }
                 case R.id.stopbt:
                     if (mTimer2 != null) {
@@ -179,9 +180,15 @@ public class MainActivity extends Activity {
                         mTimer2 = null;
                         mTimer3 = null;
                         mChronometer.stop();
+                        mChronometer.setBase(android.os.SystemClock.elapsedRealtime());
                         highDump = 0;
                         noDump = 0;
                         distance = 0;
+                        pauseFlag = false;
+                        startFlag = false;
+                        startBt.setBackgroundColor(Color.rgb(121, 192, 110));
+                        startBt.setText("計測を開始する");
+                        break;
                     }
                     break;
                 default:
